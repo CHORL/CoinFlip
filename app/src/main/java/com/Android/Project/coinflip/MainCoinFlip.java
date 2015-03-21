27299f34c1f89bda2ade7dcc735645258f7bdede
Chrizzeay <necrons501@gmail.com>
2015-03-21 18:34:23 +0100

    Additions

--- app/src/main/java/com/Android/Project/coinflip/MainCoinFlip.java
+++ app/src/main/java/com/Android/Project/coinflip/MainCoinFlip.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.media.AudioManager;
+import android.media.SoundPool;
+
+
 
 
 public class MainCoinFlip extends ActionBarActivity
