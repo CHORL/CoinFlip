a07949ac9b12fc072cd9c3e35579ad8df13f4f95
Fenrir111 <rickardhulth@yahoo.se>
2015-04-08 17:52:20 +0200

    Testar några grejer med layouten

--- app/src/main/java/com/Android/Project/coinflip/startactivity.java
+++ app/src/main/java/com/Android/Project/coinflip/startactivity.java
@@ -1,8 +1,18 @@
 package com.Android.Project.coinflip;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 /**
  * Created by rickard on 2015-04-08.
  */
 public class startactivity extends MainCoinFlip {
+
+    public void onCreate(Bundle savedInstances){
+
+    }
 
 }
