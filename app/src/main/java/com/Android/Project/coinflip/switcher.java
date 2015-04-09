2cc4e9b049aa73269f62b034cc72dbc326d63c4c
Fenrir111 <rickardhulth@yahoo.se>
2015-04-09 23:52:47 +0200

    Testar några grejer med layouten

---
+++ app/src/main/java/com/Android/Project/coinflip/switcher.java
@@ -0,0 +1,43 @@
+package com.Android.Project.coinflip;
+
+/**
+ * Created by rickard on 2015-04-09.
+ *
+ *
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.media.MediaPlayer;
+
+public class switcher extends ActionBarActivity{
+
+    @Override
+
+    public void onCreate(Bundle savedInstances){
+        super.onCreate(savedInstances);
+        setContentView(R.layout.switcher);
+
+        Button switchbutton = (Button)findViewById(R.id.button2);
+
+        switchbutton.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+        public void onClick(View view){
+                Intent intent = new Intent(switcher.this, coinflip.class);
+                startActivity(intent);
+            }
+
+        });
+
+    }
+
+
+}
