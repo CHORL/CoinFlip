b99694de0cba65644bdcb66e7f8c1855148be41a
Chrizzeay <necrons501@gmail.com>
2015-04-10 22:59:28 +0200

    Stuff.

--- app/src/main/java/com/Android/Project/coinflip/MainCoinFlip.java
+++ app/src/main/java/com/Android/Project/coinflip/MainCoinFlip.java
@@ -1,13 +1,11 @@
 package com.Android.Project.coinflip;
 
-import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.media.MediaPlayer;
 
@@ -26,24 +24,24 @@ int Ran_Number = number.randInt(); //random coin chance
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_coin_flip);
         myLO = (LinearLayout) findViewById(R.id.MainCoinLayout);
-        final ImageButton Coin = (ImageButton) findViewById(R.id.Coin);
-        Coin.setBackgroundResource(R.drawable.gold_coin);
+        final ImageButton Coin = (ImageButton) findViewById(R.id.Coin); //create coin as an ImageButton
+        Coin.setBackgroundResource(R.drawable.gold_coin); //coin is there when you start
     }
 
-    public void buttonOnClick(View Coin)
+    public void buttonOnClick(View Coin) //what happens when you click the ImageButton
     {
-        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.coin_drop_sound);
-        mediaPlayer.start();
-        Ran_Number = number.randInt();
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.coin_drop_sound); //create media player that plays coin_drop_sound
+        mediaPlayer.start(); //play media player
+        Ran_Number = number.randInt(); //generate random number when you click for which side of the coin is should show
         {
-            if (Ran_Number == 1)
+            if (Ran_Number == 1) //if the random number is 1
             {
-                Coin.setBackgroundResource(R.drawable.plain_gold_coin);
+                Coin.setBackgroundResource(R.drawable.plain_gold_coin); //display the plain coin
 
             }
-            if (Ran_Number == 2)
+            if (Ran_Number == 2) //if the random number is 2
             {
-                Coin.setBackgroundResource(R.drawable.gold_coin);
+                Coin.setBackgroundResource(R.drawable.gold_coin); //display the clover coin
             }
         }
 
